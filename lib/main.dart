2818b5fcc2f0d11d3cd617b8da0fa30f63ab2589
Guinsly Mondesir <guinslym@gmail.com>
2019-10-03 22:00:27 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home:Scaffold(
+    appBar: AppBar(
+      centerTitle: false,
+      elevation: 60,
+      titleSpacing: 10,
+      title: Text("Guinsly App"),
+      backgroundColor: Colors.black,
+      leading: IconButton(icon: Icon(Icons.menu), onPressed: (){
+        print('clicked on menu');
+      },),
+      actions: <Widget>[
+        IconButton(icon: Icon(Icons.notifications_none), onPressed: (){
+          print("Notification clicked");
+        }, color: Colors.white70),
+        IconButton(icon: Icon(Icons.search), onPressed: (){
+          print("Search Clicked");
+        }, color: Colors.white70),
+      ],
+    ),
+  ),
+)
+);
+
